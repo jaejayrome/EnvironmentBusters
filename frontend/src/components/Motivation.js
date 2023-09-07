b8823fb3abe70e2febb5af2de439849d9641402b
Jerome Goh <jerome.goh.jg@gmail.com>
2023-09-07 11:52:10 +0800

    fixed motivation in the frontend

--- frontend/src/components/Motivation.js
+++ frontend/src/components/Motivation.js
@@ -8,35 +8,34 @@ export default function Motivation() {
                 <img src = {EnvironmentLogo}  alt = "" className="object-contain w-full sm:w-1/2 sm:h-1/2"/>
                 <Box className="mx-auto py-5 text-center"> 
                     <span className="inline-block text-4xl font-roboto "> Waste Management </span>
-                    <span className="block text-4xl font-roboto text-green-900"> Revolutionised. </span>
+                    <span className="block text-4xl font-roboto text-green-900"> Connundrum. </span>
 
-                    <span className="block mt-10 ml-2 mr-2 first_paragraph"> Climate change is the defining issue of our time and generation.
-                    It has altered
-                    weather patterns, affected food and water supplies, and caused rising sea levels.
-                    As a low-lying island citystate, it is an <span style={{fontWeight: "bold", fontSize: "20px"}}>existential threat </span>   
-                    for Singapore.  </span>
+                    <div className="flex flex-col text-left space-y-8 px-4 mt-4 sm:mt-0 sm:px-0"> 
 
-                    <span className="block mt-5 ml-2 mr-2second_paragraph">
-                        According to the Global Alliance for Incinerator Alternatives (GAIA), the waste sector is responsible for 20% of global methane emissions. 
-                        Yet, waste reduction solutions like recycling can reduce the sector's total emissions by 84%. 
-                        
-                    </span>
+                        <div> 
+                            <span className="text-2xl font-roboto"> Context <span className="text-3xl"> &#x1F914; </span> </span> 
+                            <ul> Climate change is a critical issue with <span className="font-bold"> global </span> consequences <span>&#x1F30F; </span>.</ul>
+                            <ul> Singapore <span> &#x1F1F8;&#x1F1EC; </span>, as a low-lying state <span> &#x1F30A;</span>, faces an existential threat from it. </ul>
+                        </div>
 
-                    <span className="block ml-2 mr-2 mt-5">
-                        In waste recycling, sorting of recyclable items is a critical stage. 
-                        However, existing workflows in Singapore rely on humans 
-                        for sorting, which is not only slow but labour-intensive, creating significant inefficiencies. Additionally, the National Environment Agency (NEA) 
-                        has found that almost 60% of the waste thrown into recycling bins cannot be recycled due to contamination, mainly from food and liquid waste. 
-                    </span>
-
-                    <span className="block ml-2 mr-2 mt-5">
-        
-                        As such, our solution aims to streamline the sorting processes of recyclable waste and significant increase the efficiency of
-                        operations in Material Recovery Facilities (MRFs) by utilising deep learning models to identify and automatically sort the waste collected in recycling bins. 
-
-                        At the same time, the sorting of waste items into their respective material categories helps to reduce widespread contamination of recyclable waste, 
-                        optimising the proportion of waste successfully recycled. 
-                    </span> 
+                        <div> 
+                            <span className="text-2xl font-roboto"> Environmental Impact of Waste <span className="text-3xl"> &#x1F621; </span> </span> 
+                            <ul> The waste sector <span> &#x1F5D1; </span> contributes to <span className="text-red-900 font-bold">20% </span>of global methane emissions.</ul>
+                            <ul> Recycling <span> &#x267B;</span> can reduce waste sector emissions by up to <span className="text-red-900 font-bold">84%</span>. </ul>
+                        </div>
+                        <div> 
+                            <span className="text-2xl font-roboto"> Challenges in Recycling <span className="text-3xl"> &#x1F630; </span> </span> 
+                            <ul> Manual waste sorting <span> &#x1F477;&#x200D;&#x2642;&#xFE0F;</span> is slow <span> &#x23F3; </span>and labour-intensive<span> &#x1F4AA;</span>.</ul>
+                            <ul> Contamination from food <span> &#x1F35A; </span>and liquid <span> &#x1F9C3;</span>waste, affects recycling bins. </ul>
+                        </div>
+                        <div> 
+                            <span className="text-2xl font-roboto"> Proposed Solution <span className="text-3xl"> &#x1F4BB; </span> </span> 
+                            <ul> Implement deep learning models <span> &#x1F916; </span>for efficient waste sorting <span> &#x1F4C8; </span>.</ul>
+                            <ul> Material Recovery Facilities (MRFs) benefit <span> &#x1F44D;</span> from improved operations. </ul>
+                            <ul> Automated sorting reduces contamination <span> &#x2B07;</span>, enhancing recycling rates <span> &#x1F333;</span>. </ul>
+                        </div>
+                    </div>
+                    
                 </Box>
                 
             </Box>
